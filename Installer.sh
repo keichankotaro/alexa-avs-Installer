dc9710d6a8366f422fa9e541562c1d5427d56e09
keisukekisu <44016629+keisukekisu@users.noreply.github.com>
2019-03-09 16:17:23 +0900

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,6 @@
 cd ~
 sudo apt-get upgrade
-
+cd avs
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/setup.sh
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/genConfig.sh
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/pi.sh
