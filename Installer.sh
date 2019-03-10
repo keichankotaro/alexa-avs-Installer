97a937bc8de4a85b268f14cb86f479f657627d16
keisukekisu <44016629+keisukekisu@users.noreply.github.com>
2019-03-10 11:56:25 +0900

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -5,4 +5,3 @@ wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/genConfig.sh
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/pi.sh
 sudo bash setup.sh config.json [-s 1234]
-sudo bash startsample.sh
