71faa20f64fe03a3dea720432861d4bfdc1b0a16
keisukekisu <44016629+keisukekisu@users.noreply.github.com>
2020-05-07 12:30:32 +0900

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,6 @@
 cd ~
-sudo apt-get install git
-sudo apt-get -y upgrade
+sudo apt -y install git
+sudo apt -y upgrade
 cd avs
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/setup.sh
 wget https://raw.githubusercontent.com/alexa/avs-device-sdk/master/tools/Install/genConfig.sh
